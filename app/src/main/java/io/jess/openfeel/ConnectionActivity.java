d0fcc669d4f3f4e53420caf05233fd513d6bbf96
Jieni Hou <jhou25@uwo.ca>
2015-11-14 16:01:43 -0500

    records

--- app/src/main/java/io/jess/openfeel/ConnectionActivity.java
+++ app/src/main/java/io/jess/openfeel/ConnectionActivity.java
@@ -1,13 +1,12 @@
 package io.jess.openfeel;
 
-        import android.annotation.SuppressLint;
-        import android.support.v7.app.ActionBar;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.os.Handler;
-        import android.view.MotionEvent;
-        import android.view.View;
-
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
