bbfe3d8d0dced8c99ab4ecfc48d2940c1d3ab19c
Jessica Joseph <jessjeanjo@gmail.com>
2015-11-14 01:34:45 -0500

    Create proejcts

---
+++ OpenFeel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenFeel
+//
+//  Created by Jessica Joseph on 11/14/15.
+//  Copyright (c) 2015 HackHarvard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
